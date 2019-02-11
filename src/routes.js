1a021c57901ddaeda1b5e7f21c03b3b0b7d17466
Marcus Paulo <mpaulobr@gmail.com>
2019-02-11 21:52:03 -0200

    Ajuetes no Babelrc e no arquivo de Rotas

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,27 @@
-import { StackNavigator } from "react-navigation";
+import { createAppContainer, createStackNavigator } from "react-navigation";
 import { colors } from "styles";
 
 import Main from "pages/main";
 import Search from "pages/main";
 import Album from "pages/main";
 
-const Routes = StackNavigator(
-  {
-    Main: { screen: Main },
-    Search: { screen: Search },
-    Album: { screen: Album }
-  },
-  {
-    navigationOptions: {
-      headerStyle: {
-        backgroundColor: colors.white
-      },
-      headerTintColor: colors.white,
-      headerBackTitle: null
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main: { screen: Main },
+      Search: { screen: Search },
+      Album: { screen: Album }
+    },
+    {
+      navigationOptions: {
+        headerStyle: {
+          backgroundColor: colors.white
+        },
+        headerTintColor: colors.white,
+        headerBackTitle: null
+      }
     }
-  }
+  )
 );
 
 export default Routes;
